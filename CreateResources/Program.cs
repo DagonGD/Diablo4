25c299b723bcf05c32f9b754f9c5a7f31b67dc54
antgit <iljinaa@gmail.com>
2012-09-08 00:56:31 +0300

    files

---
+++ CreateResources/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Core;
+
+namespace CreateResources
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Map map=new Map(5,5);
+            map.Save("map1.map");
+        }
+    }
+}
